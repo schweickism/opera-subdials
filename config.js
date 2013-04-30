360d9804b02e69c7479740d8b21f960c9187453b
schweickism <schweickism@hotmail.com>
2013-04-30 16:27:44 -0400

    setup

---
+++ config.js
@@ -0,0 +1,15 @@
+var subdial={
+	// Name of the sub-dial category
+	'Opera Links':[
+		// Links on this dial
+		// URI, (optional title), (optional thumbnail)
+		['http://www.opera.com/', 'Opera', 'thumbs/opera.png'],
+		['https://addons.opera.com/en/', 'Opera add-ons'],
+		['http://my.opera.com/community/']
+	],
+	'Search Engines':[
+		['http://www.google.com/'],
+		['http://www.yahoo.com/'],
+		['http://www.bing.com/']
+	]
+};
